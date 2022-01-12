8225be6944ef228732ee734533d0f04eb91268d3
SheldonVon98 <sheldonvonsr@gmail.com>
2022-01-12 09:46:00 +0800

    update

---
+++ src/ubuntu/install/tigervnc.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Install TigerVNC server"
+wget -qO- https://jaist.dl.sourceforge.net/project/tigervnc/stable/1.8.0/tigervnc-1.8.0.x86_64.tar.gz | tar xz --strip 1 -C /
